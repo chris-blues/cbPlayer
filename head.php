defac80a8d53c2dc56886f6737a182f8ca051ec8
chris_blues <chris@musicchris.de>
2015-10-30 05:09:41 +0100

    minor cleanup in example index.php and head.php

--- head.php
+++ head.php
@@ -5,9 +5,7 @@
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
   <meta http-equiv="Content-Script-Type" content="text/javascript">
   <meta http-equiv="Content-Style-Type" content="text/css">
-  <!--link rel="stylesheet" href="https://musicchris.de/musicchris.css" type="text/css"-->
   <link rel="stylesheet" href="cbplayer.css" type="text/css">
-  <!--script type="text/javascript" src="/js/jquery-2.1.4.min.js"></script-->
   <title>cbPlayer - demo</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -3,8 +3,14 @@
 <div style="width: 800px; margin: 10px auto;">
 
 <?php
-  $cbPlayer_dirname = "/baustelle/cbplayer";
-  $cbPlayer_mediadir = "../../content/media";
+
+  // where cbPlayer resides on your server
+  $cbPlayer_dirname = "/cbplayer";
+
+  // where you have the media files on your server
+  $cbPlayer_mediadir = "/content/media";
+
+  // start cbPlayer!
   include("cbplayer.php");
 ?>
 
