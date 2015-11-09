afc93c77da163b07e3bd6cbedcb216c2298a77f3
chris_blues <chris@musicchris.de>
2015-11-10 00:24:26 +0100

    minor change in default in cbplayer.conf.php

--- cbplayer.conf.php
+++ cbplayer.conf.php
@@ -1,7 +1,8 @@
 <?php
 // Uncomment if you don't want a download-link to be shown for the current title
 
-//      $cbPlayer_showDownload = false;
+//	$cbPlayer_showDownload = false;
+
 
 // Uncomment to show a little timer below the player, how long the processing of
 // the media files and the calculations for the metadata fields, essentially the
